b1907ff8cf1ed75a8f9000ad8fef6bea6a72939a
silas <709817768@qq.com>
2017-03-24 12:36:45 +0800

    email
    cretae email file

---
+++ send_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import smtplib
+from email.mime.text import MIMEText
+
+
+def send_email():
+    print "12345"
+    
+if __name__ == '__main__':
+    send_email() 
